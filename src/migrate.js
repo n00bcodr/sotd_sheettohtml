b41ae133d8aa8459300cc8b2e7ca60f03929f429
Pavan Thanuj Upadrasta <49611451+n00bcodr@users.noreply.github.com>
2024-11-01 17:52:27 +0000

    Update migrate.js
    Add background style transparent

--- src/migrate.js
+++ src/migrate.js
@@ -51,6 +51,11 @@ const generateHTMLFile = (rowData) => {
 <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+
+  body {
+    background: transparent !important;
+  }
+
 /* width */
 ::-webkit-scrollbar {
   width: 10px;
